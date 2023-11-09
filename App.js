642e0203dbaf63e5d69cc93332008ea24a3ac15d
MosesK <mosesziongo@gmail.com>
2023-11-09 17:15:23 +0200

    style input

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { View, Text, ImageBackground } from "react-native";
 import { style } from "./App.style";
-import hotBackground from "./assets/cold.png"
+import hotBackground from "./assets/cold.png";
+import { InputTemperature } from "./components/InputTemperature/InputTemperature";
 
 export default function App() {
   return (
@@ -9,9 +10,7 @@ export default function App() {
         <View>
           <Text>Temperature</Text>
         </View>
-        <View>=
-          <Text>Input</Text>
-        </View>
+        <InputTemperature defaultValue={"12"}/>
         <View>
           <Text>Button</Text>
         </View>
--- App.style.js
+++ App.style.js
@@ -3,7 +3,8 @@ import { StyleSheet } from "react-native";
 export const style = StyleSheet.create({
      container : {
         flex : 1,
-        justifyContent : "center"
+        justifyContent : "center",
+        padding : 20,
      },
      workspace : {
         height : 450,
