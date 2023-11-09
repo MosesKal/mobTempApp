eba354acc2ac15b7ca1ba3d282e8925242fd520b
MosesK <mosesziongo@gmail.com>
2023-11-09 15:38:05 +0200

    setup main Layout

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import { View, Text } from "react-native";
+import { style } from "./App.style";
 
 export default function App() {
-  return <>
-    <View>
-      <Text>Hello world!</Text>
-    </View>
-  </>;
+  return (
+    <>
+      <View style={style.container}>
+        <View style={style.workspace}>
+          <View>
+            <Text>Temperature</Text>
+          </View>
+          <View>
+            <Text>Input</Text>
+          </View>
+          <View>
+            <Text>Button</Text>
+          </View>
+        </View>
+      </View>
+    </>
+  );
 }
