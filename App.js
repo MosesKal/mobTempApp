1ff30c7e5dd8e8238823ccff31d3a50b146038d7
MosesK <mosesziongo@gmail.com>
2023-11-09 16:14:34 +0200

    background image

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import { style } from "./App.style";
+import hotBackground from "./assets/cold.png"
 
 export default function App() {
   return (
-    <>
-      <View style={style.container}>
-        <View style={style.workspace}>
-          <View>
-            <Text>Temperature</Text>
-          </View>
-          <View>
-            <Text>Input</Text>
-          </View>
-          <View>
-            <Text>Button</Text>
-          </View>
+    <ImageBackground style={style.container} source={hotBackground}>
+      <View style={style.workspace}>
+        <View>
+          <Text>Temperature</Text>
+        </View>
+        <View>=
+          <Text>Input</Text>
+        </View>
+        <View>
+          <Text>Button</Text>
         </View>
       </View>
-    </>
+    </ImageBackground>
   );
 }
